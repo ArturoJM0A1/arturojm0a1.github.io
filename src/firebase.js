7f6e3c7b873da3cd8790780e1ea40b9ad97b6eee
ArturoJM0A1 <juarezmonroyarturo574@gmail.com>
2026-03-02 20:02:59 -0600

    agregar comentario firebase 3

--- src/firebase.js
+++ src/firebase.js
@@ -1,23 +1,17 @@
-
-
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCoo5aLu9fqW2-Cwz8i_JbWCFtbTvAmeAc",
-  authDomain: "comentarioscv-fc57e.firebaseapp.com",
-  projectId: "comentarioscv-fc57e",
-  storageBucket: "comentarioscv-fc57e.firebasestorage.app",
-  messagingSenderId: "59407502615",
-  appId: "1:59407502615:web:9ab4239163c65ddf723b52",
-  measurementId: "G-F92Z7E5G37"
+  apiKey: "AIzaSyD8q4tw8ugcfCJI3hsz-rOwzZnM-BOUgCg",
+  authDomain: "comentarioscvajm.firebaseapp.com",
+  projectId: "comentarioscvajm",
+  storageBucket: "comentarioscvajm.firebasestorage.app",
+  messagingSenderId: "456154483789",
+  appId: "1:456154483789:web:49d0da60ee011558d5e004"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
